fc75183ba5c564df0327ba083a5ff88c5fe37f81
Harmeet Singh <hsbaliwork@gmail.com>
2022-05-17 02:37:35 +0530

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+	plugins: ['react', 'prettier', 'react-hooks', '@typescript-eslint'],
+	rules: {
+		'import/extensions': 'off',
+		'react/react-in-jsx-scope': 0,
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+		'prettier/prettier': [
+			2,
+			{
+				printWidth: 140,
+				useTabs: true,
+				tabWidth: 4,
+				semi: true,
+				singleQuote: true,
+				bracketSpacing: true,
+				bracketSameLine: false,
+				arrowParens: 'always',
+				endOfLine: 'auto',
+			},
+		],
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+};
---
+++ src/routes/DefaultRoutes.js
@@ -0,0 +1,23 @@
+import { lazy } from 'react';
+
+// project imports
+import DefaultLayout from '../components/layouts/DefaultLayout';
+import Loadable from '../components/loaders/Loadable';
+import TopLoader from '../components/loaders/TopLoader';
+
+const HomePage = Loadable(lazy(() => import('../pages/HomePage')), TopLoader);
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const DefaultRoutes = {
+    path: '',
+    element: <DefaultLayout />,
+    children: [
+        {
+            path: '',
+            element: <HomePage />
+        },
+    ]
+};
+
+export default DefaultRoutes;
